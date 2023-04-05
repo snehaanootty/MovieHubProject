153f8142127146ecabe8f3e6c950d7c330d9cfe6
sajay <sajaykannan10@gmail.com>
2023-04-05 10:33:33 +0530

    added a new model reviews

--- MovieHubNovember/myapp/models.py
+++ MovieHubNovember/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -29,6 +30,15 @@ class Movies(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Reviews(models.Model):
+    movie=models.ForeignKey(Movies,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    comment=models.CharField(max_length=200)
+    rating=models.PositiveIntegerField()
+    
+
+
     
 
 
